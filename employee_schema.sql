04d82acded9cba177ed42f89bf047b60b09dd240
hpolowczyk <50588161+hpolowczyk@users.noreply.github.com>
2019-07-28 23:21:29 -0400

    ERD and SQL complete

---
+++ employee_schema.sql
@@ -0,0 +1,94 @@
+-- Table Schema for CSV files
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/bPJP0x
+
+-- If tables already exist, drop them:
+DROP TABLE dept_emp;
+DROP TABLE dept_manager;
+DROP TABLE departments;
+DROP TABLE salaries;
+DROP TABLE titles;
+DROP TABLE employees;
+
+-- Creation of all tables
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR   NOT NULL,
+    "dept_name" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "dept_emp" (
+    "id" SERIAL  NOT NULL,
+    "emp_no" INT   NOT NULL,
+    "dept_no" VARCHAR   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "dept_manager" (
+    "id" SERIAL   NOT NULL,
+    "dept_no" VARCHAR   NOT NULL,
+    "emp_no" INT   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "employees" (
+    "emp_no" INT   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "gender" VARCHAR   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "id" SERIAL   NOT NULL,
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "titles" (
+    "id" SERIAL   NOT NULL,
+    "emp_no" INT   NOT NULL,
+    "title" VARCHAR   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "id"
+     )
+);
+
+-- Add foreign keys to tables
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
